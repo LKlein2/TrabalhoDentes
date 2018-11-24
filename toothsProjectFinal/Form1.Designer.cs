0e1f93b3e1298c8871ffc634177aaa19c4b6f0ac
dudaeitelven <dudaeitelven@gmail.com>
2018-11-24 15:22:57 -0200

    Comeco Tela Login

--- toothsProjectFinal/Form1.Designer.cs
+++ toothsProjectFinal/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace toothsProjectFinal
 {
-    partial class Form1
+    partial class FrmLogin
     {
         /// <summary>
         /// Variável de designer necessária.
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FrmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "FrmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DentistaApp";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
