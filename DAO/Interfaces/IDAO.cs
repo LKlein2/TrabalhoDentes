d47888b632260777b9635743d1f53ac4be6806c2
Lucas <Lklein2@ucs.br>
2018-11-24 17:04:02 -0200

    Segundo commit

---
+++ DAO/Interfaces/IDAO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO.Interfaces
+{
+    public interface IDAO<T>:IDisposable
+    {
+        T Inserir(T model);
+
+        void Atualizar(T model);
+
+        bool Remover(T model);
+
+        T LocalizarPorCodigo(params Object[] Keys);
+
+        Collection<T> ListarTudo();
+    }
+}
