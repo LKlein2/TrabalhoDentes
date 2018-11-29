b02ec7c3606c618543c7be663aff08ce5ba7767b
Eduardo Eitelven <eduardo.eitelven@hotmail.com>
2018-11-28 22:51:55 -0200

    INICIADO AGENDADAO

---
+++ DAO/Classes/AgendaDAO.cs
@@ -0,0 +1,40 @@
+﻿using DAO.Interfaces;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO.Classes
+{
+    class AgendaDAO : IDAO<Agenda>, IDisposable
+    {
+        private IConnection connection;
+
+        public AgendaDAO(IConnection connection)
+        {
+            this.connection = connection;
+        }
+
+        public void Atualizar(Agenda model)
+        {
+            throw new NotImplementedException(); // colocar sql
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+
+        public Agenda Inserir(Agenda model)
+        {
+            throw new NotImplementedException(); // colocar sql
+        }
+
+        public bool Remover(Agenda model)
+        {
+            throw new NotImplementedException(); // colocar sql
+        }
+    }
+}
